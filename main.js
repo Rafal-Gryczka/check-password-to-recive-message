8696eb449e2e5c0962dc5fa4f77f7c0ae4571d85
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-05-07 20:27:26 +0200

    Add version 1

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const input = document.querySelector("input");
-const passwords = ['jedEN', 'DwA'];
-const messages = ["super", "działa!"]
+const div = document.querySelector('div');
+const passwords = ['oNe', 'TwO'];
+const messages = ["super", "great!"]
 
 const showMessage = (e) => {
+    div.textContent = '';
+    const text = e.target.value;
+    passwords.forEach((password, index) => {
+        if (password.toUpperCase() === text.toUpperCase()) {
+            div.textContent = messages[index];
+            e.target.value = '';
+        }
+    })
 
 }
 
 input.addEventListener("input", showMessage)
+
+input.addEventListener('focus', (e) => {
+    e.target.classList.add("active");
+})
+input.addEventListener('blur', (e) => {
+    e.target.classList.remove("active");
+})
