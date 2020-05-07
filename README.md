04d4d6cc8f214d4fd00961e9c23a84788e1dc9f3
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-05-07 19:43:13 +0200

    Initialize repositiry

---
+++ README.md
@@ -0,0 +1,1 @@
+"#check password\ Check if password matches with the ones in array, if so - show a message.\ Letters' size is irrelevant" 
---
+++ main.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const passwords = ['jedEN', 'DwA'];
+const messages = ["super", "działa!"]
+
+const showMessage = (e) => {
+
+}
+
+input.addEventListener("input", showMessage)
